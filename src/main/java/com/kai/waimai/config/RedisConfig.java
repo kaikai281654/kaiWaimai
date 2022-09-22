d891a5e4d58b681eeddb0e67ace09a9d88fcde5c
kaikai281654 <1282398114@qq.com>
2022-09-22 14:19:22 +0800

    添加redis组件

---
+++ src/main/java/com/kai/waimai/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.kai.waimai.config;
+
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object,Object> redisTemplate =new RedisTemplate<>();
+        //改掉默认的spring序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+
+
+
+}
