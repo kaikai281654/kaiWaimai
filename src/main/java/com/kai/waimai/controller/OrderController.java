0143c14c5ac0189c4c4e7501130082e2c6d846bc
kaikai281654 <1282398114@qq.com>
2022-10-04 18:43:26 +0800

    添加管理界面订单功能

--- src/main/java/com/kai/waimai/controller/OrderController.java
+++ src/main/java/com/kai/waimai/controller/OrderController.java
@@ -4,13 +4,16 @@ package com.kai.waimai.controller;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.kai.waimai.dto.OrdersDto;
-import com.kai.waimai.entity.Category;
 import com.kai.waimai.entity.Orders;
 import com.kai.waimai.entity.R;
 import com.kai.waimai.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -34,6 +37,47 @@ public class OrderController {
         Page<OrdersDto> ordersDtoPage = orderService.userPage(page,pageSize);
         return R.success(ordersDtoPage);
     }
+
+
+
+
+    @GetMapping("/page")
+    public R<Page> getpage(Integer page, Integer pageSize){
+
+        Page<Orders> pageInfo=new Page(page,pageSize);
+
+        Page<Orders> dtoPage=new Page<>();
+
+
+
+        LambdaQueryWrapper<Orders> queryWrapper=new LambdaQueryWrapper();
+
+        queryWrapper.orderByDesc(Orders::getCheckoutTime);
+
+        orderService.page(pageInfo, queryWrapper);
+
+        BeanUtils.copyProperties(pageInfo,dtoPage,"records");
+
+        List<Orders> records = pageInfo.getRecords();
+
+        List<Orders> list= records.stream().map((item) -> {
+
+            OrdersDto ordersDto = new OrdersDto();
+
+            BeanUtils.copyProperties(item, ordersDto);
+
+            String consignee = item.getConsignee();
+            if(consignee!=null){
+                ordersDto.setUserName(consignee);
+            }
+            return ordersDto;
+        }).collect(Collectors.toList());
+
+        dtoPage.setRecords(list);
+
+        return R.success(dtoPage);
+    }
+
 
 
 
